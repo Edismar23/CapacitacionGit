9fadc8f5fa25d1413b4151de9d8a34489602a471
Edismar Vargas Peraz <ext_edvargas@Falabella.cl>
2019-02-19 18:39:08 -0300

    bla bla bla

--- src/main/java/com/ejemplo/NavigateCommandsExample.java
+++ src/main/java/com/ejemplo/NavigateCommandsExample.java
@@ -1,3 +1,5 @@
+package com.ejemplo;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
--- src/main/java/com/ejemplo/TestYapoSample.java
+++ src/main/java/com/ejemplo/TestYapoSample.java
@@ -1,3 +1,5 @@
+package com.ejemplo;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
