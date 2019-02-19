8253e868feab3b7545ecc94d4e0831b327ca636d
Edismar Vargas Peraz <ext_edvargas@Falabella.cl>
2019-02-19 17:38:52 -0300

    mi primer comit

---
+++ src/main/java/CapacitacionGit/CapacitacionGit/App.java
@@ -0,0 +1,13 @@
+package CapacitacionGit.CapacitacionGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
